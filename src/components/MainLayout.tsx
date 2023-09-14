0664187ce771e2872426a47b3b9cd94e25b77395
Juan Marco <jmmarco@gmail.com>
2023-09-14 15:20:38 +0200

    Set default text for Layout component

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router'
 
 export default function Layout() {
   return (
-    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 text-gray-900">
       <Outlet />
     </div>
   )
