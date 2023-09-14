48c37a5f8cae73af10f24487e7248bfebb5ecb98
Juan Marco <jmmarco@gmail.com>
2023-09-14 15:47:25 +0200

    Restyle ErrorPage

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,17 +1,31 @@
 import { useRouteError } from 'react-router-dom'
+import Layout from './MainLayout'
+import { XCircleIcon } from '@heroicons/react/20/solid'
 
 export default function ErrorPage() {
   const error: unknown = useRouteError()
   console.error(error)
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        {(error as Error)?.message ||
-          (error as { statusText?: string })?.statusText}
-      </p>
-    </div>
+    <Layout>
+      <div className="rounded-md bg-red-50 p-4 mt-10">
+        <div className="flex">
+          <div className="flex-shrink-0">
+            <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+          </div>
+          <div className="ml-3">
+            <h3 className="text-sm font-medium text-yellow-800">
+              Sorry, an unexpected error has occurred.
+            </h3>
+            <div className="mt-2 text-sm text-yellow-700">
+              <p>
+                {(error as Error)?.message ||
+                  (error as { statusText?: string })?.statusText}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
   )
 }
