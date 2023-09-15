164a13c44c98b3eaec7ef083b79dc41afe57de7f
Juan Marco <jmmarco@gmail.com>
2023-09-15 10:35:50 +0200

    Extend tailwind config to accomodate Figma design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,37 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      backgroundColor: {
+        circle: {
+          alert: '#FFF4F4',
+          blue: '#506CF0',
+          primary: '#1C2226',
+          secondary: '#E6E8EC',
+          grey: {
+            light: '#E3E5E9',
+            medium: '#F4F4F7',
+          },
+        },
+      },
+      borderColor: {
+        circle: {
+          alert: '#DC462A',
+          primary: '#8A94A6',
+        },
+      },
       colors: {
-        default: '#1D2839',
+        circle: {
+          alert: '#D72C0D',
+          secondary: '#404F5E',
+          grey: {
+            shade: {
+              dark: '#1D2839',
+              medium: '#687387',
+              light: '#8594AA',
+              placeholder: '#8D9CAE',
+            },
+          },
+        },
       },
     },
   },
