c94c74e9365cd80d960d856066afde0119d32f70
Juan Marco <jmmarco@gmail.com>
2023-09-15 13:36:53 +0200

    Simplify extended properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,34 +4,31 @@ export default {
   theme: {
     extend: {
       backgroundColor: {
-        circle: {
-          alert: '#FFF4F4',
-          blue: '#506CF0',
-          primary: '#1C2226',
-          secondary: '#E6E8EC',
-          grey: {
-            light: '#E3E5E9',
-            medium: '#F4F4F7',
-          },
+        alert: '#FFF4F4',
+        blue: '#506CF0',
+        primary: '#1C2226',
+        secondary: '#E6E8EC',
+        grey: {
+          light: '#E3E5E9',
+          medium: '#F4F4F7',
+          dark: '#F7F9FB',
         },
       },
       borderColor: {
-        circle: {
-          alert: '#DC462A',
-          primary: '#8A94A6',
-        },
+        alert: '#DC462A',
+        primary: '#8A94A6',
+        secondary: '#E6E8EC',
       },
       colors: {
-        circle: {
-          alert: '#D72C0D',
-          secondary: '#404F5E',
-          grey: {
-            shade: {
-              dark: '#1D2839',
-              medium: '#687387',
-              light: '#8594AA',
-              placeholder: '#8D9CAE',
-            },
+        alert: '#D72C0D',
+        primary: '#8A94A6',
+        secondary: '#404F5E',
+        grey: {
+          shade: {
+            dark: '#1D2839',
+            medium: '#687387',
+            light: '#8594AA',
+            placeholder: '#8D9CAE',
           },
         },
       },
