76df6eefe2768d325551195053ba1278523ef28f
Juan Marco <jmmarco@gmail.com>
2023-09-18 08:55:17 +0200

    Group custom colors into single object

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+
+const circle = {
+  alert: {
+    text: '#D72C0D',
+    border: '#DC462A',
+    background: 'FFF4F4',
+  },
+  blue: {
+    900: '#506CF0',
+    800: '#5f7cf3',
+    700: '#6f8cf6',
+    600: '#7f9bf8',
+    500: '#91aafa',
+    400: '#a2b8fc',
+    300: '#b4c7fe',
+    200: '#c6d5ff',
+    100: '#d9e3ff',
+    50: '#ecf1ff',
+  },
+  grey: {
+    shade: {
+      dark: '#1D2839',
+      medium: '#687387',
+      light: '#8594AA',
+    },
+  },
+  placeholder: '#8D9CAE',
+}
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundColor: {
-        alert: '#FFF4F4',
-        'circle-blue': '#506CF0',
+        circle,
         primary: '#1C2226',
         secondary: '#E6E8EC',
         grey: {
@@ -15,7 +43,7 @@ export default {
         },
       },
       borderColor: {
-        alert: '#DC462A',
+        circle,
         primary: '#8A94A6',
         secondary: '#E6E8EC',
       },
@@ -23,15 +51,11 @@ export default {
         alert: '#D72C0D',
         primary: '#8A94A6',
         secondary: '#404F5E',
-        grey: {
-          shade: {
-            dark: '#1D2839',
-            medium: '#687387',
-            light: '#8594AA',
-            placeholder: '#8D9CAE',
-          },
-        },
+        circle,
       },
+    },
+    ringColor: {
+      circle,
     },
   },
   plugins: [require('@tailwindcss/forms')],
