38c3f6eb662b1ccbb29f218fa0cac6d434ba5844
Juan Marco <jmmarco@gmail.com>
2023-09-18 09:53:57 +0200

    Leverage Button components with intent

--- src/components/events/EventHeader.tsx
+++ src/components/events/EventHeader.tsx
@@ -3,20 +3,22 @@ import Button from '../buttons/Button'
 
 export default function EventHeader() {
   return (
-    <header className="bg-white border-b border-secondary mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="flex items-center h-[66px]">
-        <Button type="button" className="ml-auto p-[26px] pr-0">
+    <header className="mx-auto border-b border-secondary bg-white px-4 sm:px-6 lg:px-8">
+      <div className="flex h-[66px] items-center">
+        <Button
+          type="button"
+          intent="secondary"
+          className="ml-auto border-0 p-2 hover:bg-transparent"
+        >
+          <span className="sr-only">Close</span>
           <XMarkIcon className="h-5 w-5 text-primary" aria-hidden="true" />
         </Button>
       </div>
       <div className="mx-auto max-w-2xl sm:px-6 lg:px-8">
-        <div className="flex items-center h-[165px] max-w-3xl mx-auto justify-between">
-          <h1 className="font-bold text-[40px]">Event form</h1>
-          <Button
-            type="button"
-            className="capitalize border border-secondary py-2.5 px-[18px] rounded-md"
-          >
-            edit
+        <div className="mx-auto flex h-[165px] max-w-3xl items-center justify-between shadow-none">
+          <h1 className="text-[40px] font-bold">Event form</h1>
+          <Button type="button" intent="secondary" size="small">
+            Edit
           </Button>
         </div>
       </div>
