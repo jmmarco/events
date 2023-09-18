3e58b7f3bc2beed9c29fd6bcadac9ac47b508736
Juan Marco <jmmarco@gmail.com>
2023-09-18 09:23:01 +0200

    Remove background color and add horizontal padding for smaller devices

--- src/components/events/EventMain.tsx
+++ src/components/events/EventMain.tsx
@@ -2,8 +2,6 @@ import { ChildrenProps } from '../../types/global'
 
 export default function EventMain({ children }: ChildrenProps) {
   return (
-    <main className="bg-grey-dark mx-auto max-w-2xl sm:px-6 lg:px-8">
-      {children}
-    </main>
+    <main className="mx-auto max-w-2xl px-4 sm:px-6 lg:px-8">{children}</main>
   )
 }
