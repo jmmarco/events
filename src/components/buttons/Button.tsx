d3d7f14f24f4183447f39a0612bc120a84bdf20e
Juan Marco <jmmarco@gmail.com>
2023-09-18 12:42:38 +0200

    Separate buttonStyles into single file

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,6 +1,6 @@
 import { forwardRef } from 'react'
 import { cn } from '../../helpers/utils'
-import { cva } from 'class-variance-authority'
+import { buttonStyles } from './buttonStyles'
 
 interface ButtonProps {
   className?: string
@@ -9,44 +9,6 @@ interface ButtonProps {
   size?: 'small' | 'medium'
   type: 'submit' | 'button' | 'reset'
 }
-
-const buttonStyles = cva(
-  [
-    'font-semibold',
-    'border',
-    'rounded-md',
-    'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-circle-blue-900',
-  ],
-  {
-    variants: {
-      intent: {
-        primary: [
-          'bg-circle-blue-900',
-          'text-white',
-          'border-transparent',
-          'hover:bg-circle-blue-800',
-        ],
-        // **or**
-        // primary: "bg-blue-500 text-white border-transparent hover:bg-blue-600",
-        secondary: [
-          'bg-white',
-          'text-secondary',
-          'border-secondary',
-          'hover:bg-gray-100',
-        ],
-      },
-      size: {
-        small: ['text-sm', 'py-2', 'px-8'],
-        medium: ['text-base', 'py-2.5', 'px-6'],
-      },
-    },
-
-    defaultVariants: {
-      intent: 'primary',
-      size: 'medium',
-    },
-  },
-)
 
 export type Ref = HTMLButtonElement
 
