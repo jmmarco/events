a9918e243e596ad0d77aa298d8bdf0b7826b3892
Juan Marco <jmmarco@gmail.com>
2023-09-20 10:34:11 +0200

    Add EventCard component and render it inside Events

---
+++ src/components/cards/EventCard.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router'
+import { EventProps } from '../../types/events'
+
+interface EventCardProps {
+  event: EventProps
+}
+
+export default function EventCard({ event }: EventCardProps) {
+  const navigate = useNavigate()
+  const imgSrc =
+    event.location === 'Virtual'
+      ? '/event-virtual-logo.svg'
+      : '/event-in-person.svg'
+
+  return (
+    <div
+      className="flex max-w-xs cursor-pointer items-center space-x-4 rounded-xl bg-white p-6 shadow-lg"
+      onClick={() => {
+        console.log('fired')
+        navigate(`/events/${event.id}`)
+      }}
+    >
+      <div className="shrink-0">
+        <img className="h-12 w-12" src={imgSrc} alt="Event Logo" />
+      </div>
+      <div>
+        <p className="text-xl font-medium text-black">{event.name}</p>
+        <p className="text-slate-500">{event.duration} hours</p>
+      </div>
+    </div>
+  )
+}
--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -5,6 +5,7 @@ import EventMain from '../components/events/EventMain'
 import { EventProps } from '../types/events'
 import api from '../helpers/api'
 import { VITE_API_URL } from '../constants'
+import EventCard from '../components/cards/EventCard'
 
 export default function Events() {
   const [events, setEvent] = useState<EventProps[] | null>(null)
@@ -21,7 +22,9 @@ export default function Events() {
     <div className="h-full bg-circle-grey-background">
       <EventHeader headingTitle="Events" />
       <EventMain>
-        <pre>{JSON.stringify(events, null, 2)}</pre>
+        <div className="flex justify-between gap-4 py-20">
+          {events?.map((event) => <EventCard key={event.id} event={event} />)}
+        </div>
       </EventMain>
     </div>
   )
