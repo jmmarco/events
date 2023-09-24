efb866929f7626b441e011fe208ac107dc6eb238
Juan Marco <jmmarco@gmail.com>
2023-09-24 14:31:50 +0200

    Pass LoaderContext provider along with value

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from 'react-router'
 import { ChildrenProps } from '../types/global'
+import Loader from './Loader'
+import LoaderContext from './LoaderContext'
+import useLoading from '../hooks/useLoading'
 
 export default function Layout({ children }: ChildrenProps) {
+  const value = useLoading()
+
   return (
-    <div className="h-screen text-circle-grey-shade-dark">
-      {children}
-      <Outlet />
+    <div className="relative h-full text-circle-grey-shade-dark">
+      <LoaderContext.Provider value={value}>
+        {children}
+        <Outlet />
+        <Loader />
+      </LoaderContext.Provider>
     </div>
   )
 }
