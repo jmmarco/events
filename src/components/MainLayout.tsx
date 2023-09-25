d6d38a26570207bbbf5a7f768ea632fc0ed0e577
Juan Marco <jmmarco@gmail.com>
2023-09-25 09:34:04 +0200

    Use ErrorBoundary to wrap entire app structure

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,17 +3,26 @@ import { ChildrenProps } from '../types/global'
 import Loader from './Loader'
 import LoaderContext from './LoaderContext'
 import useLoading from '../hooks/useLoading'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorPage from './ErrorPage'
 
 export default function Layout({ children }: ChildrenProps) {
   const value = useLoading()
 
   return (
-    <div className="relative h-full text-circle-grey-shade-dark">
-      <LoaderContext.Provider value={value}>
-        {children}
-        <Outlet />
-        <Loader />
-      </LoaderContext.Provider>
-    </div>
+    <ErrorBoundary
+      FallbackComponent={ErrorPage}
+      onError={(error, info) => {
+        console.log(`[Boundary]`, error, info)
+      }}
+    >
+      <div className="relative h-full text-circle-grey-shade-dark">
+        <LoaderContext.Provider value={value}>
+          {children}
+          <Outlet />
+          <Loader />
+        </LoaderContext.Provider>
+      </div>
+    </ErrorBoundary>
   )
 }
