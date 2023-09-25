20adc730dbf6ef6e108c6ab40b8a9a557eae1fa1
Juan Marco <jmmarco@gmail.com>
2023-09-25 09:39:17 +0200

    Show error boundary if error is truthy

--- src/components/events/Event.tsx
+++ src/components/events/Event.tsx
@@ -6,20 +6,26 @@ import { useState } from 'react'
 import { EventProps } from '../../types/events'
 import { VITE_API_URL } from '../../constants'
 import useFetch from '../../hooks/useFetch'
+import { useErrorBoundary } from 'react-error-boundary'
 
 type ActionProps = 'create' | 'edit' | 'view'
 
 export default function Event() {
   const { eventId } = useParams()
+  const { showBoundary } = useErrorBoundary()
   const navigate = useNavigate()
   const [action, setAction] = useState<ActionProps>('view')
   const singleEventEndpointUrl = `${VITE_API_URL}/events/${eventId}`
-  const { data: event } = useFetch<EventProps | null>({
+  const title = action === 'edit' ? 'Edit Event' : 'Event Details'
+
+  const { data: event, error } = useFetch<EventProps | null>({
     url: singleEventEndpointUrl,
     initialState: null,
   })
 
-  const title = action === 'edit' ? 'Edit Event' : 'Event Details'
+  if (error) {
+    showBoundary(error)
+  }
 
   return (
     <div className="bg-circle-grey-background">
--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -4,14 +4,20 @@ import { EventProps } from '../types/events'
 import { VITE_API_URL } from '../constants'
 import EventCardList from '../components/cards/EventCardList'
 import useFetch from '../hooks/useFetch'
+import { useErrorBoundary } from 'react-error-boundary'
 
 export default function Events() {
+  const { showBoundary } = useErrorBoundary()
   const eventsEndpointUrl = `${VITE_API_URL}/events`
 
-  const { data: events } = useFetch<EventProps[] | null>({
+  const { data: events, error } = useFetch<EventProps[] | null>({
     url: eventsEndpointUrl,
     initialState: null,
   })
+
+  if (error) {
+    showBoundary(error)
+  }
 
   return (
     <div className="h-full bg-circle-grey-background">
