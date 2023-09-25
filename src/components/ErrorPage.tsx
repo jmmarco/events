5d1d322711fd5cb3b5ebd9f11fd6ca65884a1549
Juan Marco <jmmarco@gmail.com>
2023-09-25 09:33:25 +0200

    Refactor ErrorPage to accept error prop

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,31 +1,32 @@
 import { useRouteError } from 'react-router-dom'
-import Layout from './MainLayout'
 import { XCircleIcon } from '@heroicons/react/20/solid'
 
-export default function ErrorPage() {
-  const error: unknown = useRouteError()
-  console.error(error)
+interface ErrorPageProps {
+  error?: Error
+}
+
+export default function ErrorPage({ error }: ErrorPageProps) {
+  const routerError: unknown = useRouteError()
+  console.error(error || routerError)
 
   return (
-    <Layout>
-      <div className="mx-auto mt-10 max-w-2xl rounded-md bg-red-50 p-4">
-        <div className="flex">
-          <div className="flex-shrink-0">
-            <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
-          </div>
-          <div className="ml-3">
-            <h3 className="text-sm font-medium text-yellow-800">
-              Sorry, an unexpected error has occurred.
-            </h3>
-            <div className="mt-2 text-sm text-yellow-700">
-              <p>
-                {(error as Error)?.message ||
-                  (error as { statusText?: string })?.statusText}
-              </p>
-            </div>
+    <div className="mx-auto mt-10 max-w-2xl rounded-md bg-red-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-yellow-800">
+            Sorry, an unexpected error has occurred.
+          </h3>
+          <div className="mt-2 text-sm text-yellow-700">
+            <p>
+              {(error || (routerError as Error))?.message ||
+                (routerError as { statusText?: string })?.statusText}
+            </p>
           </div>
         </div>
       </div>
-    </Layout>
+    </div>
   )
 }
