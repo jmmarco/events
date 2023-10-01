69b030b8ff2687faa7827a1f575be215b114e7b8
Juan Marco <jmmarco@gmail.com>
2023-10-01 12:30:01 +0200

    Add EventMainProps interface

--- src/components/events/EventMain.tsx
+++ src/components/events/EventMain.tsx
@@ -1,7 +1,11 @@
 import { cn } from '../../helpers/utils'
-import { ChildrenProps } from '../../types/global'
 
-export default function EventMain({ children, className }: ChildrenProps) {
+export interface EventMainProps {
+  children?: React.ReactNode
+  className?: string
+}
+
+export default function EventMain({ children, className }: EventMainProps) {
   return (
     <main className={cn('mx-auto max-w-2xl px-4 sm:px-6 lg:px-8', className)}>
       {children}
