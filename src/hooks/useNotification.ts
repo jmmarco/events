3cfe43fa2c8699097db4e7088e8e237225c5b80f
Juan Marco <jmmarco@gmail.com>
2023-10-03 19:23:01 +0200

    Add JSDoc comment for useNotification hook

--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -2,6 +2,18 @@ import { useMemo, useState } from 'react'
 
 export type NotificationType = 'success' | 'error' | 'warning'
 
+/**
+ * Hook for managing pop up notifications.
+ *
+ * @returns {object} Notification state and functions.
+ * @property {boolean} show - Whether the notification is currently displayed.
+ * @property {function(boolean)} setShow - Function to toggle the notification display.
+ * @property {NotificationType} notificationType - The type of notification ('success', 'error' or 'warning').
+ * @property {function(NotificationType)} setNotificationType - Function to set the notification type.
+ * @property {string} notificationText - The text content of the notification.
+ * @property {function(string)} setNotificationText - Function to set the notification text.
+ */
+
 export default function useNotification() {
   const [show, setShow] = useState(false)
   const [notificationType, setNotificationType] =
