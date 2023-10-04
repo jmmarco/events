6a0376a36472addf27df30773a7062162a4dc9d8
Juan Marco <jmmarco@gmail.com>
2023-10-04 15:43:07 +0200

    Error message is now more descriptive

--- tests/page-not-found.spec.ts
+++ tests/page-not-found.spec.ts
@@ -1,17 +1,25 @@
 import { test, expect } from '@playwright/test'
 
+const nonExistingRoute = '/something-that-does-not-exist'
 test.beforeEach(async ({ page }) => {
-  await page.goto('/something-that-does-not-exist')
+  await page.goto(nonExistingRoute)
 })
 
 test.describe('Navigating to unknown page', () => {
-  test('context', async ({ page }) => {
+  test('Error heading', async ({ page }) => {
     const errorHeading = page.getByRole('heading', {
       name: 'Sorry, an unexpected error has occurred.',
     })
-    const errorMessage = page.getByText('Not found')
 
     await expect(errorHeading).toBeVisible()
+  })
+
+  test('Error message', async ({ page }) => {
+    const errorMessage = page.locator('p')
+
     await expect(errorMessage).toBeVisible()
+    await expect(errorMessage).toContainText(
+      `No route matches URL "${nonExistingRoute}"`,
+    )
   })
 })
