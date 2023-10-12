3553493efb6a1bb978ff29e4b5fd185ca9d10647
Juan Marco <jmmarco@gmail.com>
2023-10-12 09:29:09 +0200

    Add api service object literal

---
+++ src/api/apiService.ts
@@ -0,0 +1,13 @@
+import { EventProps } from '../types/events'
+import axiosInstance from './axiosInstance'
+
+const EVENTS_PATH = 'events'
+
+export const apiService = {
+  getEvent: (id: string) => axiosInstance.get(`/${EVENTS_PATH}/${id}`),
+  getEvents: () => axiosInstance.get(`/${EVENTS_PATH}`),
+  createEvent: (event: EventProps) =>
+    axiosInstance.post(`/${EVENTS_PATH}`, event),
+  editEvent: (id: string, event: EventProps) =>
+    axiosInstance.put(`/${EVENTS_PATH}/${id}`, event),
+}
