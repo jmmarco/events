194996dff60562ff0344c4c2cd88dd4e374e241d
Juan Marco <jmmarco@gmail.com>
2023-10-13 09:59:48 +0200

    Add actionReducer

---
+++ src/reducers/actionReducer.ts
@@ -0,0 +1,18 @@
+import { Reducer } from 'react'
+import { EventActionType } from '../pages/events/Event'
+
+type ActionState = EventActionType
+
+export type Action = { type: 'SET_ACTION'; payload: ActionState }
+
+// Create the reducer function
+const actionReducer: Reducer<ActionState, Action> = (state, action) => {
+  switch (action.type) {
+    case 'SET_ACTION':
+      return action.payload
+    default:
+      return state
+  }
+}
+
+export default actionReducer
