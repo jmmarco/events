c58bc60c04e822a0d6f76c390a94239c3281d6d2
Juan Marco <jmmarco@gmail.com>
2024-05-22 12:19:02 +0200

    Refactor useCreate event to leverage mutation function

--- src/hooks/events/mutations/useCreateEvent.ts
+++ src/hooks/events/mutations/useCreateEvent.ts
@@ -1,38 +1,40 @@
-import { AxiosError } from 'axios'
 import { apiService } from '@api/apiService'
-import { useContext, useState } from 'react'
-import { sleep } from '@helpers/utils'
-import LoaderContext from '@context/LoaderContext'
 import { EventProps } from '@customTypes/events/EventProps'
+import { useMutation } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
 
-interface UseCreateEventResponse {
-  data: EventProps | null
-  loading: boolean
-  error: Error | null
-  mutate: (event: EventProps) => Promise<EventProps | null>
-}
+async function createEvent(payload: EventProps) {
+  try {
+    const response = await apiService.createEvent(payload)
 
-export const useCreateEvent = (): UseCreateEventResponse => {
-  const [data, setData] = useState<EventProps | null>(null)
-  const [error, setError] = useState<Error | null>(null)
-  const { loading, setLoading } = useContext(LoaderContext)
-  const SIMULATED_NETWORK_DELAY = 2500
-
-  const createEvent = async (event: EventProps) => {
-    setLoading(true)
-    try {
-      const response = await apiService.createEvent(event)
-      setData(response.data)
-      await sleep(SIMULATED_NETWORK_DELAY)
-      setLoading(false)
+    if (response.status === 201) {
       return response.data
-    } catch (error) {
-      const axiosError = error as AxiosError
-      setError(axiosError)
-      setLoading(false)
-      return null
+    } else {
+      throw new Error(
+        `Failed to create event: Received status code ${response.status}`,
+      )
+    }
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      // Handle Axios-specific errors
+      throw new Error(
+        `Failed to create event: ${
+          error.response?.data?.message || error.message
+        }`,
+      )
+    } else {
+      // Handle other errors
+      throw new Error('Failed to create event: An unexpected error occurred')
     }
   }
+}
 
-  return { mutate: createEvent, data, loading, error }
+export const useCreateEvent = () => {
+  return useMutation({
+    mutationFn: (payload: EventProps) => createEvent(payload),
+    meta: {
+      errorMessage: `Failed to create event.`,
+      successMessage: `Successfully  created event.`,
+    },
+  })
 }
